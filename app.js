95ce0684b98d252b2c41288dfa1ae53becc7bfb7
cYpHeR <github.com/cypher-nullbyte>
2021-03-27 11:36:47 +0530

    .gitignore

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //jshint esversion:6
+const express=require('express');
+const ejs=require('ejs');
+
+
+const app =express();
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(express.json());
+
+
+app.listen(3000,()=>{
+    console.log('Server started on port 3000...');
+});
